a624136801da2de700f2268cc8ec48120bf7ad3b
jeffvilla <jvilla125@gmail.com>
2023-06-13 11:41:40 -0700

    include a background url image for the sign up page

--- src/pages/App/Signup/Signup.js
+++ src/pages/App/Signup/Signup.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './Signup.css'
 
 export default function Signup(){
     return(
         <>
+        <div className="signup">
         <h1>
             This will be the signup page
         </h1>
+        </div>
         </>
     )
 }
